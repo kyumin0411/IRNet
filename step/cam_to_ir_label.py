0abc43e97102223d3703f196c193ba0411709a9c
kyumin0411 <rbalsgood@gmail.com>
2023-11-30 15:37:35 +0900

    debug

--- step/cam_to_ir_label.py
+++ step/cam_to_ir_label.py
@@ -17,7 +17,7 @@ def _work(process_id, infer_dataset, args):
     infer_data_loader = DataLoader(databin, shuffle=False, num_workers=0, pin_memory=False)
 
     for iter, pack in enumerate(infer_data_loader):
-        # import pdb; pdb.set_trace()
+        import pdb; pdb.set_trace()
         img_name = voc12.dataloader.decode_int_filename(pack['name'][0])
         img = pack['img'][0].numpy()
         cam_dict = np.load(os.path.join(args.cam_out_dir, img_name + '.npy'), allow_pickle=True).item()
